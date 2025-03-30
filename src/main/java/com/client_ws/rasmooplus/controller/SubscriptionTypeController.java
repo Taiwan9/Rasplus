dfb2f083f4ae8ade994cd58471d910b9f3592cb6
taiwa <clownser1994@gmail.com>
2025-03-30 16:04:16 -0300

    feat: Melhorando nossas Handlers com o ErrorResponseDto, criando o metodo Delete

--- src/main/java/com/client_ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client_ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -42,4 +42,10 @@ public class SubscriptionTypeController {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.update(id,dto));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        subscriptionTypeService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
 }
