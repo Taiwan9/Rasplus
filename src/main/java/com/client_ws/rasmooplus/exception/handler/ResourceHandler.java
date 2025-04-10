2b4dce5db833c5236a7daf9c48c5bb12d977c733
taiwa <clownser1994@gmail.com>
2025-04-10 17:23:39 -0300

    feat: Buscando o usuário, verificando a assinatura e o businessexception

--- src/main/java/com/client_ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/client_ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -2,6 +2,7 @@ package com.client_ws.rasmooplus.exception.handler;
 
 import com.client_ws.rasmooplus.dto.error.ErrorResponseDto;
 import com.client_ws.rasmooplus.exception.BadRequestException;
+import com.client_ws.rasmooplus.exception.BusinessException;
 import com.client_ws.rasmooplus.exception.NotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -31,6 +32,15 @@ public class ResourceHandler {
                 .message(b.getMessage())
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponseDto> businessException(BusinessException b){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponseDto.builder()
+                .message(b.getMessage())
+                .httpStatus(HttpStatus.CONFLICT)
+                .statusCode(HttpStatus.CONFLICT.value())
                 .build());
     }
 
--- src/main/java/com/client_ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
+++ src/main/java/com/client_ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
@@ -1,18 +1,37 @@
 package com.client_ws.rasmooplus.service.impl;
 
 import com.client_ws.rasmooplus.dto.PaymentProcessDto;
+import com.client_ws.rasmooplus.exception.BusinessException;
+import com.client_ws.rasmooplus.exception.NotFoundException;
+import com.client_ws.rasmooplus.model.User;
+import com.client_ws.rasmooplus.repository.UserRepository;
 import com.client_ws.rasmooplus.service.PaymentInfoService;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class PaymentInfoServiceImpl implements PaymentInfoService {
 
-    PaymentInfoServiceImpl(){}
+    private final UserRepository userRepository;
+
+    PaymentInfoServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
 
     @Override
     public Boolean process(PaymentProcessDto dto) {
-        //Verificar usuario por id
+
+        //Verificar usuario por id verifica se existe assinatura
+        var userOpt = userRepository.findById(dto.getUserPaymentInfo().getId());
+        if(userOpt.isEmpty()){
+            throw new NotFoundException("Usuario não encontrado");
+        }
+        User user = userOpt.get();
+        if(Objects.nonNull(user.getSubscriptionType())){
+            throw new BusinessException("Usuario já possui assinatura")
+        }
         //salvar as informações de pagament
         //criar ou atualizar usuario raspay
         //criar o pedido de pagamento
