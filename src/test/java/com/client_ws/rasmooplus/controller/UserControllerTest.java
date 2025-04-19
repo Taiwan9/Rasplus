50c4e608feb37e9a76d188aed8d7e62c01359967
taiwa <clownser1994@gmail.com>
2025-04-19 12:47:41 -0300

    Feat:TDD: Criando controller para envio do MultpartFile

---
+++ src/test/java/com/client_ws/rasmooplus/controller/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.client_ws.rasmooplus.controller;
+
+import com.client_ws.rasmooplus.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles(profiles = "test")
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    void given_uploadPhoto_when_recieveMultPartFile_then_return200Ok() throws Exception {
+        FileInputStream stream =  new FileInputStream("src/test/resources/static/imgJava.png");
+        MockMultipartFile multipartFile = new MockMultipartFile("file","imgJava.png", MediaType.MULTIPART_FORM_DATA_VALUE, stream);
+
+        mockMvc.perform(multipart("/user/1/upload-photo")
+                .file(multipartFile))
+                .andExpect(status().isOk())
+        ;
+    }
+}
